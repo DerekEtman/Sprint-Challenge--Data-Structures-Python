2394573d16c9ab17bd390f8348cbd58b35882d86
Derek Etman <dereketman@gmail.com>
2020-01-31 13:56:56 -0800

    + 1 off error.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -19,33 +19,63 @@ class RingBuffer:
 
         # Checking to see if ring is at max capacity.
         
-        if self.size_tracker > self.capacity:
-            # delete current 
-            self.storage.delete(self.current)
-            self.size_tracker -= 1
-            # add item to tail
-            self.storage.add_to_tail(item)
-            self.size_tracker += 1
+        if self.size_tracker == self.capacity:
+            # delete head of the storage
+            self.storage.remove_from_head()
+            # self.size_tracker -= 1
 
-        self.current = item
-        self.storage.add_to_head(self.current)
-        self.storage.head = self.current
+        self.storage.add_to_tail(item)
+
+        if self.size_tracker > 0:
+            self.current = self.storage.tail
+
         self.size_tracker += 1
 
-        # print(f"Size Tracker: {self.size_tracker}, current item: {item}")
-        print(self.current)
+        print(f"storage head: {self.storage.head.value}, adding item {item}")
+        # print(f"self.current: {self.current}")
 
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
+   
+        # if self.current:
+        #     list_buffer_contents.append(self.current.value)
+        #     # Keep repeating, updating self.current with the next value
+        #     while self.current.next:
+        #         self.current = self.current.next
+        #         list_buffer_contents.append(self.current.value)
+        # compare that we have the same amount of elements in our list vs storage
 
-    
-        if len(list_buffer_contents) < self.capacity:
-            # list_buffer_contents.append(self.current)
-            list_buffer_contents.append(self.current)
+        element = self.storage.head
+
+        # if len(list_buffer_contents) < self.storage.length:
+        while element:
+          list_buffer_contents.append(element.value)
+          element = element.next
 
         return list_buffer_contents
+
+
+
+# ______________________ old code _________________________
+
+
+        # If there is a current object: add it to the list
+        # if self.current:
+        #     list_buffer_contents.append(self.current.value)
+        #     # Keep repeating, updating self.current with the next value
+        #     while self.current.next:
+        #         self.current = self.current.next
+        #         list_buffer_contents.append(self.current.value)
+        # # compare that we have the same amount of elements in our list vs storage
+        # if len(list_buffer_contents) < self.storage.length:
+        #   element = self.storage.head
+        #   list_buffer_contents.append(element.value)
+        #   while element.next and len(list_buffer_contents) < self.storage.length:
+        #     element = element.next
+        #     list_buffer_contents.append(element.value)
+
 
 # ----------------Stretch Goal-------------------
 
